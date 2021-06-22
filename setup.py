b5430c9d3d5b0e83b1e84dd0758e12b47c5906fe
Paul-Emmanuel Chouc <pechouc@gmail.com>
2021-06-22 12:55:14 +0200

    Styling set-up and corrections

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='destination_based_sales',
       version="1.0",
       description="Project Description",
       packages=find_packages(),
-      test_suite = 'tests',
+      test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
       scripts=['scripts/destination_based_sales-run'],
